69b6b7dd4ed498b298d4ba3c7af2ba81f04e06bd
R krishna prasad <64321626+krishnaPrasad2000@users.noreply.github.com>
2021-07-15 09:36:49 +0530

    Delete GenerateNewCountry.m

--- MOMICA/GenerateNewCountry.m
+++
@@ -1,8 +0,0 @@
-function NewCountry = GenerateNewCountry(Num,edpp)
-
-    VarMinMatrix= repmat(edpp.varMin,Num,1);
-    VarMaxMatrix= repmat(edpp.varMax,Num,1);
-    NewCountry = (VarMaxMatrix - VarMinMatrix).*rand(size(VarMinMatrix))+ VarMinMatrix;
-   
-end
-
